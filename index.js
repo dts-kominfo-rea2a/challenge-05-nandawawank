a87b340e02501302adeb583c20585a49159f9918
Nanda Wawan K <nanda@terralogiq.com>
2022-06-05 20:20:47 +0700

    JS Callback - Nanda Wawan K

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names, callback) => {
+  let result = [];
+  const sortedNames = callback(names);
+  
+  for (let index = 0; index < sortedNames.length; index++) {
+    result.push(`${index + 1}. ${sortedNames[index]}`);
+  }
+
+  return result;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (names) => {
+  return names.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (names) => {
+  return names.reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
